b2f6773aaa98af72a333f17fe1cb5e09eeef6305
zupkoroland <37579768+zupkoroland@users.noreply.github.com>
2019-01-14 18:48:41 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <script src="/angularjs/angular.js"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.5/angular.min.js"></script>
     <script type="text/javascript">
+        //Angular modul létrehozása.
         var app = angular.module('angApp', []);
-
+        
+        
+        //Az angular controller létrehozása
         app.controller('angCtrl', function ($http, $scope, $interval){
+            
+            //Az adatok lekérdezése GET-en keresztül $http-vel.
             function getData() {
                 $http({
                     method: 'GET',
@@ -18,13 +22,15 @@
                     $scope.data = result.data;
                 })
             }
-
+            
+            //Lekérdezést másodpercenként elvégzi.
             $interval(getData, 1000);
         });
     </script>
     <title>Document</title>
 </head>
 <body ng-app="angApp" ng-controller="angCtrl" ng-cloak>
+    <!--Adatok megjelenítéshez szükséges table tag-->
     <table border="1px">
         <thead>
             <tr>    
@@ -34,6 +40,7 @@
             </tr>
         </thead>
         <tbody>
+            <!-- Az angular repeat-el kilistázom a getData()-val kapott adatokat -->
             <tr ng-repeat="item in data">
                 <td>{{item.card}}</td>
                 <td>{{item.name}}</td>
