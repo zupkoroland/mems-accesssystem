095efa7b9aa64ddc9a87b2c36e33e1bbdf7a2f18
zupkoroland <37579768+zupkoroland@users.noreply.github.com>
2019-01-14 18:55:22 +0100

    Update led2.py

--- RaspberryPi/led2.py
+++ RaspberryPi/led2.py
@@ -2,11 +2,20 @@ import RPi.GPIO as GPIO
 import time
 
 def ledOut2(port):
+    #GPIO mód beállítása BOARD-ra.
     GPIO.setmode(GPIO.BOARD)
+    
+    #A port változón kapott érték OUT-ra állítása.
     GPIO.setup(port, GPIO.OUT)
 
+    #A porton lévő LED megvilágítása.
     GPIO.output(port, GPIO.HIGH)
+    
+    #A futás szüneteltése egy másodpercig.
     time.sleep(1)
-
+    
+    #A porton lévő LED feszültségét elvesszi.
     GPIO.output(port, GPIO.LOW)
+    
+    #Felszabadítja a GPIO-t.
     GPIO.cleanup()
